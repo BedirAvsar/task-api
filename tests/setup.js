015a31d8ab4c58e3b144982e01d631c378b11119
Bedir Avsar <bediravsar@gmail.com>
2026-03-21 12:56:21 +0300

    add ci postgre setıp

--- tests/setup.js
+++ tests/setup.js
@@ -1,5 +1,24 @@
 const pool = require("../db");
 
+beforeAll(async () => {
+  await pool.query(`
+    CREATE TABLE IF NOT EXISTS users (
+      id UUID PRIMARY KEY,
+      email TEXT UNIQUE NOT NULL,
+      password TEXT NOT NULL
+    );
+  `);
+
+  await pool.query(`
+    CREATE TABLE IF NOT EXISTS tasks (
+      id UUID PRIMARY KEY,
+      title TEXT NOT NULL,
+      created_at TIMESTAMP,
+      user_id UUID REFERENCES users(id)
+    );
+  `);
+});
+
 afterAll(async () => {
   await pool.end();
 });
